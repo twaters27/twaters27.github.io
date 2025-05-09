d518f096a647eebc664f4c26d453f2895370075e
Trevor Waters <twater623@west-mec.org>
2025-05-09 14:23:07 -0700

    done

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,12 @@ import "./App.css";
 import { LoadingScreen } from "./components/LoadingScreen";
 import { Navbar } from "./components/Navbar";
 import { MobileMenu } from "./components/MobileMenu";
-import "./index.css";
 import { Home } from "./components/sections/Home";
 import { About } from "./components/sections/About";
+import { Projects } from "./components/sections/Projects";
+import "./index.css";
+import { Contact } from "./components/sections/Contact";
+
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
   const [menuOpen, setMenuOpen] = useState(false);
@@ -22,6 +25,8 @@ function App() {
         <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         <Home />
         <About />
+        <Projects />
+        <Contact />
       </div>
     </>
   );
--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -1,65 +1,113 @@
+import { RevealOnScroll } from "../RevealOnScroll";
+
 export const About = () => {
   const frontendSkills = [
     "React",
-    "JavaScript",
+    "Vue",
     "TypeScript",
-    "Tailwind CSS",
+    "TailwindCSS",
     "Bootstrap",
+    "JavaScript",
   ];
+
   const backendSkills = ["Node.js", "Express", "MongoDB", "MySQL"];
+
   return (
     <section
       id="about"
       className="min-h-screen flex items-center justify-center py-20"
     >
-      <div className="max-w-3xl mx-auto px-4">
-        <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
-          About Me
-        </h2>
-        <div className=" rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all">
-          <p className="text-gray-300 mb-b">
-            Dedicated devloper with experience in building full-stack responsive
-            applications and innovating on the fly.
-          </p>
-          <div className=" grid grid-cols-1 md:grid-cols-2 gap-6">
-            <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
-              <h3 className="text-xl font-bold mb-4"> Frontend</h3>
-              <div className=" flex flex-wrap gap-2">
-                {frontendSkills.map((tech, key) => (
-                  <span
-                    key={key}
-                    className="bg-blue-500 text-white px-3 py-1 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition-all duration-200"
-                  >
-                    {tech}
-                  </span>
-                ))}
-              </div>
-            </div>
+      <RevealOnScroll>
+        <div className="max-w-3xl mx-auto px-4">
+          <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
+            {" "}
+            About Me
+          </h2>
 
-            <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
-              <h3 className="text-xl font-bold mb-4"> Backend</h3>
-              <div className=" flex flex-wrap gap-2">
-                {backendSkills.map((tech, key) => (
-                  <span
-                    key={key}
-                    className="bg-blue-500 text-white px-3 py-1 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition-all duration-200"
-                  >
-                    {tech}
-                  </span>
-                ))}
+          <div className="rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all">
+            <p className="text-gray-300 mb-6">
+              Passionate developer with expertise in building scalable web
+              applications and creating innovative solutions.
+            </p>
+
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+              <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                <h3 className="text-xl font-bold mb-4"> Frontend</h3>
+                <div className="flex flex-wrap gap-2">
+                  {frontendSkills.map((tech, key) => (
+                    <span
+                      key={key}
+                      className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
+                                    hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition
+                    "
+                    >
+                      {tech}
+                    </span>
+                  ))}
+                </div>
+              </div>
+
+              <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                <h3 className="text-xl font-bold mb-4"> Backend</h3>
+                <div className="flex flex-wrap gap-2">
+                  {backendSkills.map((tech, key) => (
+                    <span
+                      key={key}
+                      className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
+                                    hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition
+                    "
+                    >
+                      {tech}
+                    </span>
+                  ))}
+                </div>
               </div>
             </div>
           </div>
+
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8">
             <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
-              <h3 className="text-xl font-bold mb-4">Education</h3>
+              <h3 className="text-xl font-bold mb-4"> 🏫 Education </h3>
+              <ul className="list-disc list-inside text-gray-300 space-y-2">
+                <li>
+                  <strong>900 Hour Coding Program</strong> - West-MEC Central
+                </li>
+                <li>
+                  <strong>Estrella Mountain Community College</strong> - Dual
+                  Enrollment (West-MEC)
+                </li>
+                <p>
+                  Relavant Coursework: Computer Information Systems,
+                  Internet/Web Dev, Introduction to Javascript, Web Scripting
+                  with PHP, and Object-Oriented Programming
+                </p>
+
+                <li>
+                  <strong>High School Diploma</strong> - Canyon View High School
+                </li>
+                <p>
+                  Relavant Coursework: Coding 1-2, Coding Honors 3-4, AP Comp
+                  Sci Principals (3)
+                </p>
+              </ul>
             </div>
-            <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
-              <h3 className="text-xl font-bold mb-4">Work Experience</h3>
-            </div>
+            {/* <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
+              <h3 className="text-xl font-bold mb-4"> 💼 Work Experience </h3>
+              <div className="space-y-4 text-gray-300">
+                <div>
+                  <h4 className="font-semibold"> </h4>
+                  <p></p>
+                </div>
+
+                <div>
+                  <h4 className="font-semibold"> </h4>
+                  <p></p>
+                </div>
+              </div>
+            </div> */}
           </div>
         </div>
-      </div>
+      </RevealOnScroll>
     </section>
   );
 };
