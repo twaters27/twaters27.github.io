720c5405868ef432f77285c680a8cfc0e047933e
Trevor Waters <twater623@west-mec.org>
2025-05-16 13:38:14 -0700

    deploying

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,10 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "./",
+  server: {
+    host: '0.0.0.0',
+    port: 5173
+
+  }
 })
